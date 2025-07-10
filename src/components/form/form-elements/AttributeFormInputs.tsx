8df2b7668f0781b72890a32eb5d38b95947ab12d
Anoopss <anoopviswanadh007@gmail.com>
2025-07-10 22:37:56 +0530

    create and list attribute options

--- src/components/form/form-elements/AttributeFormInputs.tsx
+++ src/components/form/form-elements/AttributeFormInputs.tsx
@@ -9,6 +9,9 @@ import Radio from '../input/Radio';
 import { ATTRIBUTE_TYPES } from '@/core/constants';
 import { formatSlug } from '@/utils/stringUtils';
 import { IAttributeListItem, IAttributeOption } from '@/core/types';
+import Button from '@/components/ui/button/Button';
+import { TrashBinIcon } from '@/icons';
+import EmptySection from '@/components/example/EmptySection';
 
 interface AttributeFormInputsProps {
   heading?: string;
@@ -68,13 +71,73 @@ const AttributeFormInputs: FC<AttributeFormInputsProps> = ({
     );
   };
 
+  const handleEditAttributeOption = (
+    keyData: string,
+    optionIndex: number,
+    option: { keyValue?: string; value?: string }
+  ) => {
+    setAllAttributes((prev: IAttributeListItem[]) =>
+      prev.map((item) => {
+        if (item.key === keyData) {
+          const newOptions = [...(item.options || [])];
+          if (!newOptions[optionIndex]) {
+            newOptions[optionIndex] = { label: '', value: '' };
+          }
+          if (option.keyValue) {
+            newOptions[optionIndex].label = option.keyValue;
+          }
+          if (option.value) {
+            newOptions[optionIndex].value = option.value;
+          }
+          return {
+            ...item,
+            options: newOptions,
+          };
+        }
+        return item;
+      })
+    );
+  };
+
+  const handleAddEmptyAttributeOption = (key: string) => {
+    setAllAttributes((prev: IAttributeListItem[]) =>
+      prev.map((item) => {
+        if (item.key === key) {
+          const newOptions = [
+            ...(item.options || []),
+            { label: '', value: '' },
+          ];
+          return {
+            ...item,
+            options: newOptions,
+          };
+        }
+        return item;
+      })
+    );
+  };
+
+  const handleDeleteAttributeOption = (index: number) => {
+    setAllAttributes((prev: IAttributeListItem[]) =>
+      prev.map((item) => {
+        if (item.options && item.options.length > index) {
+          const newOptions = [...item.options];
+          newOptions.splice(index, 1);
+          return {
+            ...item,
+            options: newOptions,
+          };
+        }
+        return item;
+      })
+    );
+  };
+
   const attributeToContentMapper = (
     key: string,
     selectedType: 'Select' | 'Number' | 'Boolean' | 'Text',
     options: IAttributeOption[] | null
   ) => {
-    console.log(options);
-
     return (
       <>
         <div className='my-3'>
@@ -111,6 +174,78 @@ const AttributeFormInputs: FC<AttributeFormInputsProps> = ({
             />
           ))}
         </div>
+        {selectedType === 'Select' && (
+          <div className='my-3'>
+            <Label>Options</Label>
+            {Array.isArray(options) && options.length ? (
+              options.map((option, index) => (
+                <div key={index + 1} className='grid grid-cols-13 gap-3 my-2'>
+                  <div className='col-span-6'>
+                    <Input
+                      type='text'
+                      placeholder='Key ..'
+                      name={`${option.label}_${index}key`}
+                      disabled={disabled}
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        handleEditAttributeOption(key, index, {
+                          keyValue: e.target.value,
+                        })
+                      }
+                      value={option.label}
+                      required
+                    />
+                  </div>
+                  <div className='col-span-6'>
+                    <Input
+                      type='text'
+                      placeholder='value..'
+                      name={`${option.label}_${index}_value`}
+                      disabled={disabled}
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        handleEditAttributeOption(key, index, {
+                          value: e.target.value,
+                        })
+                      }
+                      value={option.value}
+                      required
+                    />
+                  </div>
+                  <div className='col-span-1'>
+                    <Button
+                      size='sm'
+                      variant='outline'
+                      type='button'
+                      onClick={() => {
+                        handleDeleteAttributeOption(index);
+                      }}
+                      disabled={disabled}
+                    >
+                      <TrashBinIcon />
+                    </Button>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <EmptySection
+                heading='No Options'
+                description='Add options to your attribute'
+              />
+            )}
+            <div>
+              <Button
+                size='xm'
+                type='button'
+                className='text-blue-500 hover:text-blue-600 my-2'
+                onClick={() => {
+                  handleAddEmptyAttributeOption(key);
+                }}
+                disabled={disabled}
+              >
+                Add Option
+              </Button>
+            </div>
+          </div>
+        )}
       </>
     );
   };
