8bf5702c79a84fe65aab7b3ece474f5b4e2cc1a2
Anoopss <anoopviswanadh007@gmail.com>
2025-07-11 23:34:58 +0530

    order list for customer

--- src/app/(admin)/(others-pages)/customers/[id]/page.tsx
+++ src/app/(admin)/(others-pages)/customers/[id]/page.tsx
@@ -1,10 +1,19 @@
 'use server';
 
+import { getOrders } from '@/actions/order';
 import { getUserInfoById } from '@/actions/user';
 import PageBreadcrumb from '@/components/common/PageBreadCrumb';
+import OrderList from '@/components/Orders/OrdersList';
 import UserForm from '@/components/users/UserForm';
 import { validatePermissions } from '@/core/authentication/roleValidations';
-import { ICustomerInfo, IResponse } from '@/core/types';
+import SecureComponent from '@/core/authentication/SecureComponent';
+import {
+  ICustomerInfo,
+  IMiniOrder,
+  IPage,
+  IResponse,
+  ISearchParams,
+} from '@/core/types';
 import { redirect } from 'next/navigation';
 
 export async function generateMetadata() {
@@ -15,6 +24,7 @@ export async function generateMetadata() {
 
 export default async function EditCustomer(ctx: {
   params: Promise<{ id: string }>;
+  searchParams?: Promise<ISearchParams | null>;
 }) {
   await validatePermissions('USR:LS');
   const awaitedParam = await ctx.params;
@@ -30,6 +40,15 @@ export default async function EditCustomer(ctx: {
 
   const customer = userResponse.data;
 
+  const searchParams: ISearchParams | null = (await ctx.searchParams) || {};
+
+  const orderResponse: IResponse<IPage<IMiniOrder>> = await getOrders(
+    customer.email,
+    searchParams?.channel,
+    searchParams?.page,
+    searchParams?.size
+  );
+
   const breadCrumbItems = [
     { label: 'Customers', href: '/customers' },
     { label: customer.firstName, href: '#' },
@@ -44,6 +63,14 @@ export default async function EditCustomer(ctx: {
         items={breadCrumbItems}
       />
       <UserForm customer={customer} disableEdits={true} />
+      <SecureComponent permission='ORD:LS'>
+        <p className='text-lg font-semibold text-gray-800 dark:text-white/90 mb-4 mx-4'>
+          Orders
+        </p>
+        <div className='space-y-6'>
+          <OrderList {...(orderResponse.data || {})} />
+        </div>
+      </SecureComponent>
     </>
   );
 }
